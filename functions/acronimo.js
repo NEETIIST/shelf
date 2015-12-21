8f01718c6c05eaeffe4d09fb514c4716d0025898
joaaoleite <joaaogsleite@gmail.com>
2015-12-21 13:31:00 -0500

    web e function1

---
+++ functions/acronimo.js
@@ -0,0 +1,13 @@
+var course="Arq de Res69";
+var acronym="";
+for (i = 0; i < course.length; i++) { 
+   if (/[A-Z]/.test(course[i])  ){ 
+   	acronym += course[i];
+   } 
+} 
+alert(acronym);
+
+
+
+
+
